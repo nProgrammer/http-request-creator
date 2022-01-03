6ef05d40d1a23ca3812417adc4ef08a2db926d58
nPorgrammer <wagner.norbert277@outlook.com>
2022-01-03 11:37:50 +0100

    Created basic CLI app logic statements
    Created basic CLI app logic statements such as asking user about sending/saving request, creating body etc.

--- main.go
+++ main.go
@@ -1,35 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+
+	var url string
+	var method string
+	var option string
+	var body string
+	var header string
+
 	fmt.Println("Starting HTTP request creator")
 	fmt.Println("HTTP request URL: ")
+	fmt.Scan(&url)
 	fmt.Println("What HTTP method do you want to use: ")
+	fmt.Scan(&method)
 
 	//* HTTP REQUEST BODY
 	fmt.Println("Do you want to add body to the request? [y/n]")
+	fmt.Scan(&option)
 
-	//! if body TRUE
-	fmt.Printf("Do you want to: \n1. Type json body,\n2. Import body from json file.\n")
-	//!--- if option 1
-	fmt.Println("Body: ")
-	//!--- if option 2
-	fmt.Println("Path to json file: ")
-	//! if body FALSE
+	if ynToBool(option) {
+		fmt.Printf("Do you want to: \n1. Type json body,\n2. Import body from json file.\n")
+		if option == "1" {
+			fmt.Println("Body: ")
+			fmt.Scan(&body)
+		}
+		if option == "2" {
+			fmt.Println("Path to json file: ")
+		}
+	}
 
 	//* HTTP REQUEST HEADER
 	fmt.Println("Header: ")
+	fmt.Scan(&header)
 
 	//* HTTP REQUEST SENDING
 	fmt.Println("Do you want to send request? [y/n]")
-	//! if send TRUE
-	fmt.Println("Sending request")
-	//! if send FALSE
-	fmt.Println("Do you want to save request to file? [y/n]")
-	//!--- if save FALSE
-	fmt.Println("Closing CLI app")
-	//!--- if save TRUE
-	fmt.Println("Path to directory where do you want to save request: ")
-	//! CREATING FILE IN PATH, SAVING REQUEST
+	fmt.Scan(&option)
+	if ynToBool(option) {
+		fmt.Println("Sending request")
+	} else {
+		fmt.Println("Do you want to save request to file? [y/n]")
+		fmt.Scan(&option)
+		if ynToBool(option) {
+			fmt.Println("Closing CLI app")
+		} else if ynToBool(option) {
+			fmt.Println("Path to directory where do you want to save request: ")
+			//! CREATING FILE IN PATH, SAVING REQUEST
+		}
+	}
+}
+
+func ynToBool(option string) bool {
+	if option == "y" {
+		return true
+	} else {
+		return false
+	}
 }
