1dea9238d4c404494eafc74d0e21a7c743fafd43
nPorgrammer <wagner.norbert277@outlook.com>
2022-01-04 23:47:10 +0100

    Unabled headers

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 )
@@ -13,6 +15,7 @@ func main() {
 	var option string
 	var body string
 	var header string
+	var path string
 
 	fmt.Println("Starting HTTP request creator")
 	fmt.Println("HTTP request URL: ")
@@ -35,27 +38,29 @@ func main() {
 		}
 		if option == "2" {
 			fmt.Println("Path to json file: ")
+			fmt.Scan(&path)
+			body = loadFile(path)
 		}
 		args = append(args, "--body")
 		args = append(args, body)
 	}
 
 	//* HTTP REQUEST HEADER
-	fmt.Println("Header: ")
-	fmt.Scan(&header)
-	// args = append(args, "--header")
-	// args = append(args, header)
+	fmt.Println("Do you want to add header to the request? [y/n]")
+	fmt.Scan(&option)
+	if ynToBool(option) {
+		fmt.Println("Header: ")
+		fmt.Scan(&header)
+		args = append(args, "--header")
+		args = append(args, header)
+	}
 
 	//* HTTP REQUEST SENDING
 	fmt.Println("Do you want to send request? [y/n]")
 	fmt.Scan(&option)
 	if ynToBool(option) {
 		fmt.Println("Sending request")
-		//! HEADER nil, body nil
-		cmd := exec.Command("curl", args...)
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Run()
+		sendRequest(args)
 	} else {
 		fmt.Println("Do you want to save request to file? [y/n]")
 		fmt.Scan(&option)
@@ -73,5 +78,25 @@ func ynToBool(option string) bool {
 		return true
 	} else {
 		return false
+	}
+}
+
+func sendRequest(args []string) {
+	cmd := exec.Command("curl", args...)
+	log.Println("curl ", args)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func loadFile(path string) string {
+	b, err := ioutil.ReadFile(path)
+	checkErr(err)
+	return string(b)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
