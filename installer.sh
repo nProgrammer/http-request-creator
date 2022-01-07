663737760888cd4f03f00d30ba224b9117f80a27
nPorgrammer <wagner.norbert277@outlook.com>
2022-01-07 02:28:52 +0100

    Added to installer.sh function that is saving installer in tools directory

--- installer.sh
+++ installer.sh
@@ -7,6 +7,8 @@ go build
 mv http-request-creator request-creator
 
 mv request-creator $HOME/.http-request-creator/bin
+mkdir $HOME/.http-request-creator/tools
+cp installer.sh $HOME/.http-request-creator/tools
 
 echo "Now add to the .bashrc or .zshrc command: export PATH=\$PATH:\$HOME/.http-request-creator-bin" #FIXME - check grammar
 echo "To use http-request-creator use command: request-creator" #FIXME - check grammar
